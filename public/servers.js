ee0f92904f553f2ea5996e63def872989abd489a
hamwa03 <hampus.hjw.wallin@gmail.com>
2022-10-03 16:25:52 +0200

    don't work. unsure why

--- public/servers.js
+++ public/servers.js
@@ -1,1 +1,14 @@
-console.log('stable code');
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 8080;
+
+const server = http.createServer((req,res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    res.end('<h1>Hello from Node</h1>');
+});
+
+server.listen(port, hostname, () => {
+    console.log('Server is running');
+});
