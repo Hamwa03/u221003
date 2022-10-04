681dd1495e437630097e4fb9c6f01e6d4f173de9
hamwa03 <hampus.hjw.wallin@gmail.com>
2022-10-04 09:46:27 +0200

    added 'fs require' to call on the html file

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
+const fs = require('fs');
 const http = require('http');
 
 const hostname = '127.0.0.1';
 const port = 8080;
 
 const server = http.createServer((req,res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/html');
-    res.end('<h1>Hello from Node</h1>');
+    
+    fs.readFile('index.html', function(err, data) {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/html');
+        res.write(data);
+        res.end();
+    });
 });
 
 server.listen(port, hostname, () => {
